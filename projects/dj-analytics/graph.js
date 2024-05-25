be33fb3986341d95f00d7d8b9293dd418855fd33
nsprenkle <nathan.sprenkle@gmail.com>
2024-05-25 12:04:42 -0400

    Update dj-analytics files

--- projects/dj-analytics/graph.js
+++ projects/dj-analytics/graph.js
@@ -8,25 +8,55 @@ function getSongMeta (data, index) {
   return `${artist} - ${title}`
 }
 
+function getSetTitleSubtitle (longformTitle) {
+  titleParts = longformTitle.split(' | ')
+
+  if (titleParts.length >= 2) {
+    return [titleParts[0], titleParts[1]]
+  } else {
+    return [longformTitle, '']
+  }
+}
+
 function timelineGraph (container, songs, setMetadata) {
-  if (setMetadata.thumbnail) {
+  if (setMetadata.img) {
     const thumbnail = container
       .append('a')
       .attr('class', 'set-thumbnail-link')
       .attr('href', setMetadata.url)
       .append('img')
       .attr('class', 'set-thumbnail')
-      .attr('src', setMetadata.thumbnail)
+      .attr('src', setMetadata.img)
       .attr('title', setMetadata.title)
   }
 
   const title = container
-    .append('a')
+    .append('div')
     .attr('class', 'set-title')
-    .text(setMetadata.title)
+
+  // Split title into name and subtitle
+  let setName, setSubtitle;
+  [setName, setSubtitle] = getSetTitleSubtitle(setMetadata.title)
+
+  title.append('div')
+    .attr('class', 'set-name')
+    .text(setName)
+
+  title.append('div')
+    .attr('class', 'set-subtitle')
+    .text(setSubtitle)
 
   if (setMetadata.url !== '') {
     title.attr('href', setMetadata.url)
+  }
+
+  // Add set BPM
+  const bpm = container
+    .append('div')
+    .attr('class', 'bpm')
+
+  if (setMetadata.bpmMin && setMetadata.bpmMax) {
+    bpm.text(`${Math.round(setMetadata.bpmMin)} - ${Math.round(setMetadata.bpmMax)} BPM`)
   }
 
   // Build the timeline group
@@ -35,17 +65,42 @@ function timelineGraph (container, songs, setMetadata) {
     .attr('class', 'timeline')
 
   // Map data to divs
-  timeline.selectAll('div')
+  const song = timeline.selectAll('div')
     .data(songs)
     .join('div')
     .attr('class', 'song')
 
-  // Block order is the index
+    // Block order is the index
     .style('order', function (d, i) { return i })
 
-  // Color the rectangles with their energies
+    // Color the rectangles with their energies
     .attr('data-energy', getEnergy)
     .attr('title', getSongMeta)
+
+  // Add tooltip section
+  const tooltip = container
+    .append('div')
+    .attr('class', 'song-tooltip invisible')
+
+    // Placeholder text to avoid layout shift
+    .text('---')
+
+  song.on('mouseover click', function (d, i) {
+    // Mark the song as selected
+    container.selectAll('.song')
+      .attr('class', 'song')
+
+    song.attr('class', 'song selected')
+
+    // Hide all other tooltips
+    container.selectAll('.song-tooltip')
+      .attr('class', 'song-tooltip invisible')
+
+    // Show this tooltip
+    tooltip.attr('data-energy', getEnergy(d, i))
+      .text(`${d.Title || 'Unknown'} by ${d.Artist || 'Unknown'}`)
+      .attr('class', 'song-tooltip')
+  })
 }
 
 const container = d3.select('#my_dataviz')
@@ -70,12 +125,9 @@ const sortOrder = urlParams.get('order')
 const sortedData = setSortOrder(sortOrder, songData)
 
 for (const i in sortedData) {
-  const setMetadata = {
-    title: sortedData[i].title,
-    thumbnail: sortedData[i].img,
-    url: sortedData[i].url
-  }
-
   const songs = sortedData[i].data
+  const setMetadata = sortedData[i]
+  delete setMetadata.data
+
   timelineGraph(container, songs, setMetadata)
 }
